9987eca85383ce51eb48c0e1b1ed1c5dfe6feb91
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-09-12 05:14:43 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -12,4 +10,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-    return (result)
+    return result
