e11db833528bda228654df0c6d578449bc822965
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-09-19 04:35:46 -0700

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Module 5-rectangle
+"""Module 6-rectangle
 Defines a Rectangle class.
 """
 
 
 class Rectangle:
-    """Rectangle class defined by width and height."""
+    """Rectangle class defined by width and height.
+    Attributes:
+        number_of_instances: number of Rectangle instances,
+        increments with every instantitation,
+        decrements with every deletion
+    """
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
@@ -15,6 +22,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         """Returns an informal and nicely printable string representation
@@ -37,6 +45,7 @@ class Rectangle:
     def __del__(self):
         """Deletes a Rectangle instance."""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
