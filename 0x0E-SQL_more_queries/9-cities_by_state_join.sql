234ba144dcc20284b0513ca1aae63758276a039c
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-11-02 01:24:24 -0700

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
+Footer
+© 2022 GitHub, Inc.
