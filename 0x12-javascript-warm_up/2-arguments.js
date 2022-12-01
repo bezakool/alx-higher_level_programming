397fb8019aac4f673b87af81fecdde2b4fe87878
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-11-30 23:47:27 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else if (process.argv.length > 3) {
-  console.log('Arguments found');
-}
+console.log(
+  process.argv.length === 2
+    ? 'No argument'
+    : process.argv.length === 3
+      ? 'Argument found'
+      : 'Arguments found'
+);
