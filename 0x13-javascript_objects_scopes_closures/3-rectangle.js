0c68ec27076241ff4107f9ed6727c36536d5e0d0
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 00:56:08 -0800

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
   defining a Rectangle class with the attributes
-  height and width
-  If w or h is equal to 0 or not a positive integer,
+  width and height
+  If w or h is equal to 0 or not a positive integer
   it create an empty object
 */
 
@@ -11,6 +11,12 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
