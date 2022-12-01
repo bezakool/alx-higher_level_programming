5f0f0e41b2ea7c3e5bd77ac98f63c438606c707f
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-11-30 23:59:36 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const len = process.argv[2];
-
-if (isNaN(Math.floor(Number(len)))) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < len; i++) {
+const x = parseInt(process.argv[2]);
+if (x) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
