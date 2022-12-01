656c745b376bb52bb311aa3f348efb558d204c66
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 00:06:03 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-if (size) {
+const size = process.argv[2];
+
+if (isNaN(Math.floor(Number(size)))) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
-} else {
-  console.log('Missing size');
 }
