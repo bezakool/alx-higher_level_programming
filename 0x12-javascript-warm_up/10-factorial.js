5cbf7915c3904b6f0c62be7a07c03898fa4df8ed
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 00:15:01 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-
-function factorial (n) {
-  if (!n) {
-    return (1);
+function factorial (a) {
+  if (a === 1 || a === 0) {
+    return 1;
   }
-  return (n * factorial(n - 1));
+  return a * factorial(a - 1);
 }
 
-console.log(factorial(num));
+const a = Math.floor(Number(process.argv[2]));
+
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorial(a));
+}
