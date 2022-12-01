44117ef2b58477cedb0cb85ace4c250a83308cbb
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 00:27:27 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,19 @@
 #!/usr/bin/node
-
-function bubbleSort (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (Number(arr[j]) > Number(arr[j + 1])) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+const args = process.argv;
+let bigest = 0;
+let secBig = 0;
+let num;
+if (process.argv.length < 4) {
+  console.log('0');
+} else {
+  for (let i = 2; i < args.length; i++) {
+    num = parseInt(args[i]);
+    if (num > bigest) {
+      secBig = bigest;
+      bigest = num;
+    } else if (num > secBig) {
+      secBig = num;
     }
   }
-  return arr;
-}
-
-const myArr = process.argv.slice(2);
-
-if (myArr.length === 0 || myArr.length === 1) {
-  console.log(0);
-} else {
-  const newArr = bubbleSort(myArr);
-  console.log(newArr[newArr.length - 2]);
+  console.log(secBig);
 }
