32538f9f4af8cd09728e03f25180906a0051eeee
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 23:01:57 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,2 +1,21 @@
--- Insert states
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).filter(State.id)
+    for r in res:
+        print("{}: {}".format(r.id, r.name))
