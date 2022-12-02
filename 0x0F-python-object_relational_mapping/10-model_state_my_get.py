437e52ca02b826f263e7326429c5d71e530541f3
Bezawit Henok <bezawitkoolhenok@gmail.com>
2022-12-01 23:12:24 -0800

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """
-a script that prints the State object
-with the name passed as argument from the database hbtn_0e_6_usa
+Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <mysql username> /
+                                  <mysql password> /
+                                  <database name>
+                                  <state name searched>
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-
-if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3])
-    )
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State).filter(State.name == sys.argv[4])
-    try:
-        print(res[0].id)
-    except IndexError:
-        print('Not found')
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
